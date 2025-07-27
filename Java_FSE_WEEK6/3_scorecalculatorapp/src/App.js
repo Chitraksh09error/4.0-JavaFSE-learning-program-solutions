b7053cee6deea674319ed4e49a6c7bd03f4c251d
Chitraksh Chavan <147639202+Chitraksh09error@users.noreply.github.com>
2025-07-27 16:13:46 +0530

    Add files via upload

---
+++ Java_FSE_WEEK6/3_scorecalculatorapp/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import CalculatorScore from "./components/CalculatorScore";
+
+function App() {
+  return (
+    <CalculatorScore Name="Chitraksh" School="YCCE" Total={304} Goal={5}/>
+  );
+}
+
+export default App;
---
+++ Java_FSE_WEEK6/6_trainers_app/src/components/TrainerMock.js
@@ -0,0 +1,31 @@
+import Trainer from "./trainer";
+
+export const trainerMock = [
+new Trainer(
+  "t-arjun5",
+  "Arjun Mehta",
+  "arjun.mehta@cognizant.com",
+  "9812345678",
+  "Full Stack",
+  ["JavaScript", "Node.js", "React", "MongoDB"]
+),
+
+new Trainer(
+  "t-priya3",
+  "Priya Nair",
+  "priya.nair@cognizant.com",
+  "9823456789",
+  "Data Science",
+  ["Python", "Pandas", "NumPy", "Machine Learning"]
+),
+
+new Trainer(
+  "t-rahul9",
+  "Rahul Verma",
+  "rahul.verma@cognizant.com",
+  "9834567890",
+  "DevOps",
+  ["Docker", "Kubernetes", "Jenkins", "AWS"]
+),
+
+];
